9c9f8ef5019827550d64f923dcea6a03b7d26a88
luismy452 <101679109+luismy452@users.noreply.github.com>
2022-10-22 11:59:34 -0500

    add: encounter details

--- Glya Api test/src/models/patient.js
+++ Glya Api test/src/models/patient.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const Schema = mongoose.Schema; 
+require("./encounter");
 
 const patientSchema = new Schema({
     nombre: String,
     cedula: String,
+    encDetails: [{ type: Schema.Types.ObjectId, ref: 'Encounters' }]
 });
 
 module.exports = mongoose.model('Patient', patientSchema);
